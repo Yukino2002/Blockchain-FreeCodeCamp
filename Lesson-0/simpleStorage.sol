2e10870fb82a7a4b71b9dcca73d40fe0384e66ff
Pratik Jallan <pratikjalan11@gmail.com>
2022-09-10 19:49:09 +0530

    completed lesson 0

---
+++ Lesson-0/simpleStorage.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+
+// define solidity version
+// implement a range, a specific version, or a set of versions(^)
+pragma solidity >=0.6.0 <0.9.0;
+
+// define a contract
+contract simpleStorage {
+    // variable types: bool, string, int256, address, bytes32 etc.
+    // initialization by default sets variable to 0 
+    uint256 favouriteNumber;
+
+    // access modifiers: public, internal, external private, default state internal
+    function store(uint256 _favouriteNumber) public {
+        favouriteNumber = _favouriteNumber; 
+    }
+
+    // function type: view, pure
+    function retrieve() public view returns(uint256) {
+        return favouriteNumber;
+    }
+
+    // structures
+    // storing variables is always done in a numeric indexed manner
+    struct People {
+        uint256 favouriteNumber;
+        string name;
+    }
+
+    // dynamic arrays, static: specify size in square brackets
+    People[] public people;
+
+    // maps
+    mapping(string => uint256) public nameToFavouriteNumber;
+
+    function addPerson(string memory _name, uint256 _favouriteNumber) public {
+        people.push(People(_favouriteNumber, _name));
+        nameToFavouriteNumber[_name] = _favouriteNumber;
+    }
+}
